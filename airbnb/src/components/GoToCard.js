2e2ff875c8a2b7584b6d347176382ee924a4971e
PratilipiAich <pratilipiaich98@gmail.com>
2021-03-25 15:55:25 +0530

    Responsive search bar

--- airbnb/src/components/GoToCard.js
+++ airbnb/src/components/GoToCard.js
@@ -11,111 +11,62 @@ import SkipNextIcon from "@material-ui/icons/SkipNext";
 import img1 from "../assets/bg-1.jpg";
 import { Grid } from "@material-ui/core";
 import { Col, Row } from "react-bootstrap";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    margin: "8% 5% 0 8%",
-  },
-  row: {
-    marginBottom: "3%",
-  },
-  cover: {
-    borderRadius: "10px",
-    marginRight: "20px",
-    float: "left",
-    height:"80px",
-    width:"80px"
-  },
-}));
+import "../styles/GoToCard.css";
 
 export default function GoToCard() {
-  const classes = useStyles();
-  const theme = useTheme();
 
   return (
-    <div className={classes.root}>
+    <div className="root">
       <Grid>
-        <Row className={classes.row}>
+        <Row className="row">
           <Col>
-            <img
-              src={img1}
-              className={classes.cover}
-            />
+            <img src={img1} className="cover" />
             <p style={{ margin: "12px 10px 0 0", fontWeight: "600" }}>
               Gokarna
             </p>
             <p style={{ margin: "0 10px 0 0" }}>6.5-hour drive</p>
           </Col>
           <Col>
-            <img
-              src={img1}
-              className={classes.cover}
-            />
-            <p style={{ margin: "12px 10px 0 0", fontWeight: "600" }}>
-              Ooty
-            </p>
+            <img src={img1} className="cover" />
+            <p style={{ margin: "12px 10px 0 0", fontWeight: "600" }}>Ooty</p>
             <p style={{ margin: "0 10px 0 0" }}>5-hour drive</p>
           </Col>
           <Col>
-            <img
-              src={img1}
-              className={classes.cover}
-            />
+            <img src={img1} className="cover" />
             <p style={{ margin: "12px 10px 0 0", fontWeight: "600" }}>
               Kodaikanal
             </p>
             <p style={{ margin: "0 10px 0 0" }}>6.5-hour drive</p>
           </Col>
           <Col>
-            <img
-              src={img1}
-              className={classes.cover}
-            />
+            <img src={img1} className="cover" />
             <p style={{ margin: "12px 10px 0 0", fontWeight: "600" }}>
               Kalpetta
             </p>
             <p style={{ margin: "0 10px 0 0" }}>5-hour drive</p>
           </Col>
         </Row>
-        <Row className={classes.row}>
+        <Row className="row">
           <Col>
-            <img
-              src={img1}
-              className={classes.cover}
-            />
-            <p style={{ margin: "12px 10px 0 0", fontWeight: "600" }}>
-              Munnar
-            </p>
+            <img src={img1} className="cover" />
+            <p style={{ margin: "12px 10px 0 0", fontWeight: "600" }}>Munnar</p>
             <p style={{ margin: "0 10px 0 0" }}>7.5-hour drive</p>
           </Col>
           <Col>
-            <img
-              src={img1}
-              className={classes.cover}
-            />
+            <img src={img1} className="cover" />
             <p style={{ margin: "12px 10px 0 0", fontWeight: "600" }}>
               Varkala
             </p>
             <p style={{ margin: "0 10px 0 0" }}>10-hour drive</p>
           </Col>
           <Col>
-            <img
-              src={img1}
-              className={classes.cover}
-            />
-            <p style={{ margin: "12px 10px 0 0", fontWeight: "600" }}>
-              Hosur
-            </p>
+            <img src={img1} className="cover" />
+            <p style={{ margin: "12px 10px 0 0", fontWeight: "600" }}>Hosur</p>
             <p style={{ margin: "0 10px 0 0" }}>45-minute drive</p>
           </Col>
           <Col>
-            <img
-              src={img1}
-              className={classes.cover}
-            />
-            <p style={{ margin: "12px 10px 0 0", fontWeight: "600" }}>
-              Hampi
-            </p>
+            <img src={img1} className="cover" />
+            <p style={{ margin: "12px 10px 0 0", fontWeight: "600" }}>Hampi</p>
             <p style={{ margin: "0 10px 0 0" }}>5-hour drive</p>
           </Col>
         </Row>
--- airbnb/src/components/MainNav.js
+++ airbnb/src/components/MainNav.js
@@ -1,40 +1,125 @@
-import React, { Component } from 'react'
-import '../styles/MainNav.css'
-import LanguageOutlinedIcon from '@material-ui/icons/LanguageOutlined';
+import React, { Component } from "react";
+import "../styles/MainNav.css";
+import LanguageOutlinedIcon from "@material-ui/icons/LanguageOutlined";
 import logo from "../assets/logo-2.png";
-import MenuOutlinedIcon from '@material-ui/icons/MenuOutlined';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import MenuOutlinedIcon from "@material-ui/icons/MenuOutlined";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import SearchRoundedIcon from "@material-ui/icons/SearchRounded";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
 
 class MainNav extends Component {
-    render() {
-        return (
-            <div className="top-root">
-            <div className="nav-container">  
-                <ul style={{padding: 0}}>
-                <li className="logo-tab"><a className="navbar-brand" to="#">
-        <img
-        className="logo-2"
-          src={logo}
-          alt="airbnb"
-          
-        />
-      </a>    </li>
-                    <li className="tab">Places to stay
-                    </li>
-                    <li className="tab">Experiences</li>
-                    <li className="tab">Online experiences</li>
-                    <li ><button className="tab-2">Become a host</button></li>
-                    <li className="menu-tab"><MenuOutlinedIcon/>
-                    <p className="account-icon"><AccountCircleIcon/></p>
-                    </li>
-                    <li><button className="world"><LanguageOutlinedIcon /></button></li>
-                    
-                    
-                    </ul>
-                    </div>
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDesktop: false,
+    };
+    this.updatePredicate = this.updatePredicate.bind(this);
+  }
+
+  componentDidMount() {
+    this.updatePredicate();
+    window.addEventListener("resize", this.updatePredicate);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updatePredicate);
+  }
+
+  updatePredicate() {
+    this.setState({ isDesktop: window.innerWidth > 1100 });
+  }
+
+  render() {
+      
+    const isDesktop = this.state.isDesktop;
+
+    return (
+      <div>
+        {isDesktop ? (
+          <div className="top-root">
+            <div className="nav-container">
+              <ul>
+                <li className="logo-tab">
+                  <a className="navbar-brand" to="#">
+                    <img className="logo-2" src={logo} alt="airbnb" />
+                  </a>{" "}
+                </li>
+                <li className="tab">Places to stay</li>
+                <li className="tab">Experiences</li>
+                <li className="tab">Online experiences</li>
+                <li>
+                  <button className="tab-2">Become a host</button>
+                </li>
+                <li className="menu-tab">
+                  <MenuOutlinedIcon />
+                  {/* <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                  >
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                  </Select> */}
+                  <p className="account-icon">
+                    <AccountCircleIcon />
+                  </p>
+                </li>
+                <li>
+                  <button className="world">
+                    <LanguageOutlinedIcon />
+                  </button>
+                </li>
+              </ul>
             </div>
-        )
-    }
+            <div className="outer">
+              <div className="container">
+                <ul style={{ padding: 0 }}>
+                  <li className="head">
+                    Location <br />
+                    <p className="subhead">Where are you going?</p>
+                  </li>
+                  <li className="head">
+                    Check in
+                    <br />
+                    <p className="subhead">Add dates</p>
+                  </li>
+                  <li className="head">
+                    Check out
+                    <br />
+                    <p className="subhead">Add dates</p>
+                  </li>
+                  <li className="head" style={{ border: "none" }}>
+                    Guests
+                    <br />
+                    <p className="subhead">Add guests</p>
+                  </li>
+                  <li>
+                    <button className="search">
+                      <SearchRoundedIcon />
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className="main-root">
+            <div className="form-group has-search">
+              <span className="fa fa-search form-control-feedback"></span>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Where are you going?"
+              />
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
-export default MainNav
+export default MainNav;
